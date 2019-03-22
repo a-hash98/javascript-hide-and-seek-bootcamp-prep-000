4110f5dd59ac032529d2f933e83b0145e1ca7a03
Ay Has <ahashim983@yahoo.com>
2019-03-22 10:40:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
+function getFirstSelector(selector){
+  return document.querySelector([selector=value])
+}
 
+function nestedTarget(){
+  #id:nested{
+    return document.querySelectorAll([nested])
+
+    }
+  }
+
+
+}
+
+function increaseRankBy(n){
+
+
+}
+
+function deepestChild(){
+
+}
